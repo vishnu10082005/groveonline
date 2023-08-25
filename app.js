0cb1984f1fde8afbfc1dc3008e0062ea9aabdc86
vishnu10082005 <vishnu.preetham@kalvium.community>
2023-08-25 21:04:58 +0530

    iframe removed

---
+++ app.js
@@ -0,0 +1,6 @@
+var buyBtn = document.getElementById("buy")
+var text = document.getElementById("thank-text")
+buyBtn.onclick=()=>{
+    text.style.display="block"
+    buyBtn.style.display ="none"
+}
